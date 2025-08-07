c2ea0315fbc470dd2d92a91bf3f4d89eba964ce3
Johan PIQUET <johan.piquet@gmail.com>
2025-08-07 22:20:28 +0200

    Node.js support is fully ok.

---
+++ main/src/jopi-node-space-server.ts
@@ -0,0 +1,31 @@
+import {patch_process} from "./_process_s.ts";
+import {patch_thread} from "./_thread_s.ts";
+import {patch_fs} from "./_fs_s.ts";
+import {patch_crypto} from "./_crypto_s.ts";
+import {patch_compress} from "./_compress_s.ts";
+
+import type {ServerType} from "./__global.ts";
+import {isBunJs, isNodeJs} from "./common.ts";
+import {patch_os} from "./_os_s.ts";
+
+let serverType: ServerType = "nodejs";
+if (isBunJs()) serverType = "bunjs";
+const nodeSpace = globalThis.NodeSpace;
+
+nodeSpace.what = {
+    isNodeJS: isNodeJs(),
+    isBunJs: isBunJs(),
+    isBrowser: false,
+    isServerSide: true,
+    serverType: serverType,
+}
+
+patch_process();
+patch_thread();
+patch_fs();
+patch_crypto();
+patch_compress();
+patch_os();
+
+NodeSpace.app.declareServerSideReady();
+process.on('exit', () => NodeSpace.app.declareAppExiting());
