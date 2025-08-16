44b8a754cad15d96d63dc36cdd998d98d96b9a64
Johan PIQUET <johan.piquet@gmail.com>
2025-08-16 23:02:19 +0200

    Now Node.js don't allow reusing a digest.

--- main/src/_crypto_s.ts
+++ main/src/_crypto_s.ts
@@ -3,17 +3,14 @@ import crypto from "node:crypto";
 import type {CryptoImpl} from "./__global.ts";
 import {isBunJs} from "./common.ts";
 
-const sha256 = crypto.createHash('sha256');
-const md5 = crypto.createHash('md5');
-
 export function patch_crypto() {
     const myCrypto: CryptoImpl = {
         md5(text: string): string {
-            return md5.update(text).digest('hex');
+            return crypto.createHash('md5').update(text).digest('hex');
         },
 
         fastHash(text: string): string {
-            return sha256.update(text).digest('hex');
+            return crypto.createHash('sha256').update(text).digest('hex');
         }
     };
 
