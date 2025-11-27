2d03b9e261e2149b81c396c39d31533d823d963d
Johan PIQUET <johan.piquet@gmail.com>
2025-11-27 20:24:12 +0100

    Case where log file don't exist

--- src/jk_logs/jBundler_ifServer.ts
+++ src/jk_logs/jBundler_ifServer.ts
@@ -1,9 +1,12 @@
 import * as jk_app from "jopi-toolkit/jk_app";
 import * as jk_fs from "jopi-toolkit/jk_fs";
-import type {LogInitializer} from "./index";
+import type {LogInitializer} from "./index.ts";
 
 export function init(init: LogInitializer) {
-    let mainDir = jk_app.findPackageJsonDir();
+    const mainDir = jk_app.findPackageJsonDir();
+    const filePath = jk_fs.join(mainDir, "logConfig.json");
+
+    if (!jk_fs.isFile(filePath)) return;
     let text = jk_fs.readTextFromFileSync(jk_fs.join(mainDir, "logConfig.json"));
 
     if (!text) return;
