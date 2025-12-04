c2d405435ae26d9e600702e91240865ce9d8efa9
Johan PIQUET <johan.piquet@gmail.com>
2025-12-04 18:08:07 +0100

    Added githubDownload

---
+++ src/jk_tools/common.ts
@@ -0,0 +1,99 @@
+export function generateUUIDv4(): string {
+    if (typeof crypto !== 'undefined' && crypto.randomUUID) {
+        return crypto.randomUUID();
+    }
+
+    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        const r = Math.random() * 16 | 0;
+        const v = c === 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+    });
+}
+
+export function isUUIDv4(text: string): boolean {
+    return /^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(text);
+}
+
+export function getErrorMessage(e: unknown): string {
+    if (e instanceof Error) return e.message;
+    return "" + e;
+}
+
+export function applyDefaults<T>(source: T|undefined, defaults: T): T {
+    if (!source) source = {} as T;
+    return {...defaults, ...source};
+}
+
+/**
+ * Allow knowing the file path of the function calling us.
+ */
+export function getCallerFilePath(): string|undefined {
+    try {
+        throw new Error("");
+    } catch (e: any) {
+        let error: Error = e;
+
+        if (!error.stack) return undefined;
+
+        const stackLines = error.stack.split('\n');
+        if (stackLines.length < 4) return undefined;
+
+        // Here we have something like:
+        // at file:///Users/johan/Projets/jopijs-workspace/__tests/jopi-ui-sample/dist/mod_sample/routes/tests/test3.page.js:4:1
+        //
+        let fileUrl = stackLines[3].trim();
+
+        let idx = fileUrl.indexOf("file://");
+        fileUrl = fileUrl.substring(idx);
+
+        idx = fileUrl.lastIndexOf(":");
+        fileUrl = fileUrl.substring(0, idx);
+
+        idx = fileUrl.lastIndexOf(":");
+        if (idx!==-1) fileUrl = fileUrl.substring(0, idx);
+
+        return fileUrl;
+    }
+}
+
+export enum PriorityLevel {
+    veryLow = -200,
+    low = -100,
+    default = 0,
+    high = 100,
+    veryHigh = 200,
+}
+
+export interface ValueWithPriority<T> {
+    value: T;
+    priority: PriorityLevel;
+}
+
+export function sortByPriority<T>(values: undefined|ValueWithPriority<T>[]): undefined|(T[]) {
+    if (values === undefined) return undefined;
+
+    values.sort((a, b) => {
+        if (a.priority < b.priority) return -1;
+        if (a.priority > b.priority) return 1;
+        return 0;
+    });
+
+    return values.map(v => v.value);
+}
+
+/**
+ * Allows avoiding calling a function before n-milliseconds is elapsed.
+ */
+export class DontCallBeforeElapsed {
+    private lastTime = 0;
+
+    constructor(public readonly requireMs = 1000) {
+    }
+
+    check(): boolean {
+        let now = Date.now();
+        if (now-this.lastTime<this.requireMs) return false;
+        this.lastTime = now;
+        return true;
+    }
+}
---
+++ src/jk_tools/jBundler_ifServer.ts
@@ -0,0 +1,96 @@
+import * as fs from 'node:fs/promises';
+import * as path from 'node:path';
+
+export * from "./common.ts";
+
+export interface GithubDownloadParams {
+    /**
+     * The file or directory to download.
+     * Must be of type: https://github.com/ownerName/repoName/tree/branchName/path/to/folder
+     */
+    url: string;
+
+    /**
+     * Where to save the files.
+     * If downloading a directory: download the directory inside this folder.
+     * If downloading a file: this path is the name of the downloaded file.
+     */
+    downloadPath: string;
+
+    /**
+     * If true, then log the download progress.
+     */
+    log?: boolean;
+}
+
+/**
+ * Download a file or a directory from a GitHub repository.
+ */
+export async function githubDownload(params: GithubDownloadParams) {
+    async function downloadFile(localPath: string, contentUrl: string): Promise<void> {
+        const localDir = path.dirname(localPath);
+        const headers: HeadersInit = {};
+
+        await fs.mkdir(localDir, { recursive: true });
+
+        const response = await fetch(contentUrl, {headers: headers,});
+
+        if (!response.ok) {
+            throw new Error(`Échec du téléchargement: ${response.status} ${response.statusText}`);
+        }
+
+        const buffer = await response.arrayBuffer();
+        await fs.writeFile(localPath, Buffer.from(buffer));
+
+        if (params.log) console.log("Downloaded", contentUrl)
+    }
+
+    async function fetchAndProcessContent(currentPath: string, downloadRoot: string): Promise<void> {
+        const apiUrl = `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${currentPath}?ref=${branchName}`;
+        const headers: HeadersInit = {};
+
+        const response = await fetch(apiUrl, { headers });
+
+        if (!response.ok) {
+            throw new Error(`Échec de l'API: ${response.status} ${response.statusText}`);
+        }
+
+        const data = await response.json();
+        const isFile = !Array.isArray(data) && (data.type === 'file');
+
+        if (isFile && isFirst) {
+            await downloadFile(downloadRoot, data.download_url);
+            return;
+        }
+
+        const contents: any[] = Array.isArray(data) ? data : [data];
+        isFirst = false;
+
+        for (const item of contents) {
+            if (item.type === 'file' && item.download_url) {
+                let itemPath = item.path;
+                itemPath = itemPath.substring(pathInsideRepo.length + 1);
+                await downloadFile(path.join(downloadRoot, itemPath), item.download_url);
+            } else if (item.type === 'dir') {
+                await fetchAndProcessContent(item.path, downloadRoot);
+            }
+        }
+    }
+
+    let isFirst = true;
+
+    let url = params.url;
+    if (!url.startsWith("https://github.com/")) throw new Error("Invalid URL - Must be of type https://github.com/ownerName/repoName/tree/branchName/path/to/folder");
+
+    url = url.substring("https://github.com/".length);
+    let parts = url.split("/").reverse();
+
+    let repoOwner = parts.pop();
+    let repoName = parts.pop();
+    let tree = parts.pop();
+    if (tree!=="tree") throw new Error("Invalid URL - Must be of type https://github.com/ownerName/repoName/tree/branchName/path/to/folder");
+    let branchName = parts.pop();
+    const pathInsideRepo = parts.reverse().join("/");
+
+    await fetchAndProcessContent(pathInsideRepo, params.downloadPath);
+}
